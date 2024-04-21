d646d54f8b50b1fedb9f19676a0e918edfa0884e
Skythrew <mael.guerin@murena.io>
2024-04-21 12:49:52 +0200

    feat(logger): add `log_err` method

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,6 +2,7 @@ class Logger:
     def __init__(self, config: dict):
         self.config = config
         self.cyan = '\033[94m'
+        self.red = '\033[31m'
         self.reset = '\033[00m'
         
     def log_header(self, header: str):
@@ -14,6 +15,16 @@ class Logger:
 
         print(msg)
         print()
+
+    def log_err(self, err: str):
+        msg = self.red
+        
+        if self.config['general']['colors'] == False:
+            msg += self.reset
+
+        msg += f'ERROR - {err}{self.reset}'
+
+        print(msg)
 
     def log(self, content: str):
         print(content)
