40e4b01f73b7aac0c0f680b41b8a1fe2375605ed
Skythrew <mael.guerin@murena.io>
2024-04-23 11:19:00 +0200

    fix(db): `size`, `md5`, `dependencies` and `reverse-deps` are now in `infos.toml`

--- src/utils/db.py
+++ src/utils/db.py
@@ -14,17 +14,20 @@ def get_pkg_data(config: dict, pkg: str) -> dict | bool:
         for group in os.listdir(config['general']['dbpath'] + '/dist/' + repo['name']):
             if os.path.exists(config['general']['dbpath'] + '/dist/' + repo['name'] + '/' + group + '/' + pkg):
                 base_toml_data = tomllib.load(open(config['general']['dbpath'] + '/dist/' + repo['name'] + '/' + group + '/' + pkg + '/package.toml', 'rb'))
-                build_toml_data = tomllib.load(open(config['general']['dbpath'] + '/dist/' + repo['name'] + '/' + group + '/' + pkg + '/build.toml', 'rb'))
+                infos_toml_data = tomllib.load(open(config['general']['dbpath'] + '/dist/' + repo['name'] + '/' + group + '/' + pkg + '/infos.toml', 'rb'))
                 
                 pkg_data = base_toml_data
 
-                if 'run' in build_toml_data:
-                    pkg_data['dependencies'] = build_toml_data['run']
+                if 'run' in infos_toml_data:
+                    pkg_data['dependencies'] = infos_toml_data['run']
                 else:
                     pkg_data['dependencies'] = []
 
-                pkg_data['size'] = build_toml_data['size']
-                pkg_data['md5'] = build_toml_data['md5']
+                if 'reverse-deps' in infos_toml_data:
+                    pkg_data['reverse-deps'] = infos_toml_data['reverse-deps']
+
+                pkg_data['size'] = infos_toml_data['size']
+                pkg_data['md5'] = infos_toml_data['md5']
                 
                 return {'repo': repo, 'group': group, 'pkg_info': pkg_data}
 
