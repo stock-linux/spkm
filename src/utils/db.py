ce0b6eb22b767b14a482d5aaf1766af87935585b
Skythrew <mael.guerin@murena.io>
2024-04-25 15:19:30 +0200

    fix(lint): linting fixes

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,26 +1,33 @@
-import os, tomllib
+''' This module is here to mostly help reading data. '''
 
-def get_pkg_data(config: dict, pkg: str) -> dict | bool:
+import os
+import tomllib
+
+from typing import Literal
+
+def get_pkg_data(config: dict, pkg: str) -> dict | Literal[False]:
     '''Gets specified package information if the given package exists.
 
     :param dict config: SPKM Configuration
     :param str pkg: Package name
 
-    :return: Either the repo containing the package and the package info or False if the package does not exist
+    :return: The repo containing the package or False
     :rtype: tuple | bool
     '''
 
     for repo in config['repos']:
-        for group in os.listdir(config['general']['dbpath'] + '/dist/' + repo['name']):
-            if not os.path.exists(config['general']['dbpath'] + '/dist/' + repo['name'] + '/' + group + '/' + pkg):
+        repo_dir = config['general']['dbpath'] + '/dist/' + repo['name']
+        for group in os.listdir(repo_dir):
+            pkg_dir = repo_dir + '/' + group + '/' + pkg
+            if not os.path.exists(pkg_dir):
                 continue
 
-            with open(config['general']['dbpath'] + '/dist/' + repo['name'] + '/' + group + '/' + pkg + '/package.toml', 'rb') as base_toml:
+            with open(pkg_dir + '/package.toml', 'rb') as base_toml:
                 base_toml_data = tomllib.load(base_toml)
-            
-            with open(config['general']['dbpath'] + '/dist/' + repo['name'] + '/' + group + '/' + pkg + '/infos.toml', 'rb') as infos_toml:
+
+            with open(pkg_dir + '/infos.toml', 'rb') as infos_toml:
                 infos_toml_data = tomllib.load(infos_toml)
-            
+
             pkg_data = base_toml_data
 
             if 'run' in infos_toml_data:
@@ -33,12 +40,12 @@ def get_pkg_data(config: dict, pkg: str) -> dict | bool:
 
             pkg_data['size'] = infos_toml_data['size']
             pkg_data['md5'] = infos_toml_data['md5']
-            
+
             return {'repo': repo, 'group': group, 'pkg_info': pkg_data}
 
     return False
 
-def is_pkg_installed(config: dict, pkg: str) -> str | bool:
+def is_pkg_installed(config: dict, pkg: str) -> str | Literal[False]:
     '''Checks if the given package is installed or not.
 
     :param dict config: SPKM Configuration
@@ -47,14 +54,14 @@ def is_pkg_installed(config: dict, pkg: str) -> str | bool:
     :return: Either the package version or False if the package is not installed
     :rtype: str | bool
     '''
-    
+
     with open(config['general']['dbpath'] + '/local', 'rb') as local:
         local_data = tomllib.load(local)
 
     if pkg in local_data:
-        return local_data[pkg]
-    else:
-        return False
+        return local_data[pkg]['version']
+
+    return False
 
 def write_index_data(data: dict, filepath: str):
     ''' Writes index data to a file.
@@ -64,11 +71,11 @@ def write_index_data(data: dict, filepath: str):
     
     :return: None
     '''
-    
-    with open(filepath, 'w') as file:
+
+    with open(filepath, 'w', encoding='utf-8') as file:
         for key in data:
             file.write('[' + key + ']\n')
             for data_key in data[key]:
                 file.write(data_key + ' = ' + f'\'{data[key][data_key]}\'\n')
-            
+
             file.write('\n')
