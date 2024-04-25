685141de3982e243e873aa7eb5ebbb2ff2c394d7
Skythrew <mael.guerin@murena.io>
2024-04-25 10:53:52 +0200

    fix(lint): linting fixes

--- src/main.py
+++ src/main.py
@@ -1,27 +1,76 @@
 #!/usr/bin/env python
 
+''' Main module handling arguments. '''
+
 import argparse
 import operations
 
-from config import *
+from utils.config import get_config
 
 parser = argparse.ArgumentParser(
-                    prog='spkm',
-                    description='Stock PacKage Manager',
-                    )
+            prog='spkm',
+            description='Stock PacKage Manager',
+        )
 
 subparsers = parser.add_subparsers(dest='operation')
 
-add_parser = subparsers.add_parser('add', help='Adds some packages to your system.', description='Adds some packages to your system.')
-del_parser = subparsers.add_parser('del', help='Deletes some packages from your system.', description='Deletes some packages from your system.')
-sync_parser = subparsers.add_parser('sync', help='Sync with remote repos.', description='Sync with remote repos.')
-info_parser = subparsers.add_parser('info', help='Displays information about the given package.', description='Displays information about the given package.')
-up_parser = subparsers.add_parser('up', help='Upgrades your system.', description='Upgrades your system.')
-conf_parser = subparsers.add_parser('conf', help='Displays your SPKM configuration.', description='Displays your SPKM configuration.')
+add_parser = subparsers.add_parser(
+    'add',
+    help='Adds some packages to your system.',
+    description='Adds some packages to your system.'
+)
 
-add_parser.add_argument('packages', type=str, nargs='*', help='Packages to add')
-del_parser.add_argument('packages', type=str, nargs='*', help='Packages to delete')
-info_parser.add_argument('package', type=str, help='Package to display')
+del_parser = subparsers.add_parser(
+    'del',
+    help='Deletes some packages from your system.',
+    description='Deletes some packages from your system.'
+)
+
+sync_parser = subparsers.add_parser(
+    'sync',
+    help='Sync with remote repos.',
+    description='Sync with remote repos.'
+)
+
+info_parser = subparsers.add_parser(
+    'info',
+    help='Displays information about the given package.',
+    description='Displays information about the given package.'
+)
+
+up_parser = subparsers.add_parser(
+    'up',
+    help='Upgrades your system.',
+    description='Upgrades your system.'
+)
+
+conf_parser = subparsers.add_parser(
+    'conf',
+    help='Displays your SPKM configuration.',
+    description='Displays your SPKM configuration.'
+)
+
+
+add_parser.add_argument(
+    'packages',
+    type=str,
+    nargs='*',
+    help='Packages to add'
+)
+
+del_parser.add_argument(
+    'packages',
+    type=str,
+    nargs='*',
+    help='Packages to delete'
+)
+
+info_parser.add_argument(
+    'package',
+    type=str,
+    help='Package to display'
+)
+
 
 args = parser.parse_args()
 config = get_config()
